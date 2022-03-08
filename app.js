fa9308df899add5829570d9723b96a8192e998cc
masudrana <masudranaacb@gmail.com>
2022-03-08 09:04:43 +0600

    using prompt,confirm

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const adminBtn=()=>{
-    alert('ggv')
+   const add=confirm('are you sure')
+   console.log(add)
+   if(add===true){
+       prompt('please give your number')
+   }
+   else{
+       console.log('wrong')
+   }
 }
